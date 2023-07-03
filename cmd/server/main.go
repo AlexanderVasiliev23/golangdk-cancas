b554d383e49b74ecf48a1a135de0445a8b36bdc7
avasilev <a.vasilev@elama.ru>
2023-07-03 19:23:04 +0300

    Init

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,117 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
+	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
-	fmt.Println("🤓")
+	os.Exit(start())
+}
+
+func start() int {
+
+	ctx := context.Background()
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		time.Sleep(600 * time.Millisecond)
+
+		return fmt.Errorf("err1")
+	})
+
+	eg.Go(func() error {
+		time.Sleep(300 * time.Millisecond)
+
+		return fmt.Errorf("err2")
+	})
+
+	eg.Wait()
+
+	return 0
+
+	//logEnv := getStringOrDefault("LOG_ENV", "development")
+	//log, err := createLogger(logEnv)
+	//if err != nil {
+	//	fmt.Println("Error setting up the logger:", err)
+	//	return 1
+	//}
+	//defer func() {
+	//	// If we cannot sync, there's probably something wrong with outputting logs,
+	//	// so we probably cannot write using fmt.Println either. So just ignore the error.
+	//	_ = log.Sync()
+	//}()
+	//
+	//host := getStringOrDefault("HOST", "localhost")
+	//port := getIntOrDefault("PORT", 8080)
+	//
+	//s := server.New(server.Options{
+	//	Host: host,
+	//	Port: port,
+	//})
+	//
+	//ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	//defer stop()
+	//eg, ctx := errgroup.WithContext(ctx)
+	//
+	//eg.Go(func() error {
+	//	if err := s.Start(); err != nil {
+	//		log.Info("Error starting server", zap.Error(err))
+	//		return err
+	//	}
+	//	return nil
+	//})
+	//
+	//<-ctx.Done()
+	//
+	//eg.Go(func() error {
+	//	if err := s.Stop(); err != nil {
+	//		log.Info("Error stopping server", zap.Error(err))
+	//		return err
+	//	}
+	//	return nil
+	//})
+	//
+	//if err := eg.Wait(); err != nil {
+	//	return 1
+	//}
+	//
+	//return 0
+}
+
+func createLogger(env string) (*zap.Logger, error) {
+	switch env {
+	case "production":
+		return zap.NewProduction()
+	case "development":
+		return zap.NewDevelopment()
+	default:
+		return zap.NewNop(), nil
+	}
+}
+
+func getStringOrDefault(name, defaultV string) string {
+	v, ok := os.LookupEnv(name)
+	if !ok {
+		return defaultV
+	}
+	return v
+}
+
+func getIntOrDefault(name string, defaultV int) int {
+	v, ok := os.LookupEnv(name)
+	if !ok {
+		return defaultV
+	}
+	vAsInt, err := strconv.Atoi(v)
+	if err != nil {
+		return defaultV
+	}
+	return vAsInt
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module canvas
 
 go 1.18
+
+require github.com/go-chi/chi/v5 v5.0.8
+
+require (
+	github.com/matryer/is v1.4.1 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+)
